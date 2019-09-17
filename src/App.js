d428f245db7e3b0e59543fed9ede31d1fab61c03
JrzenonDev <jrobertoonb@gmail.com>
2019-09-16 23:39:47 -0300

    componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import Header from './components/Header';
+
+import './styles.css';
+
+// stateless component
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
